257a58c29e934106e6dab8f724cb313285f07f8b
mavl <mvaronamac@gmail.coml>
2020-03-16 23:53:47 +0100

    Add config firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoWUfK7apyBBPPLu2o9fdpKBEJQNZ3ILA",
+    authDomain: "product-hunt-6e4d3.firebaseapp.com",
+    databaseURL: "https://product-hunt-6e4d3.firebaseio.com",
+    projectId: "product-hunt-6e4d3",
+    storageBucket: "product-hunt-6e4d3.appspot.com",
+    messagingSenderId: "460323255413",
+    appId: "1:460323255413:web:c755ccae9508bf3790f43c"
+};
+export default firebaseConfig;
